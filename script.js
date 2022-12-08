87a304f8c863bc7d7b06c13a7ffdeb358ecf18de
Egekorkmz <ege.korkmaz@ug.bilkent.edu.tr>
2022-12-08 13:14:35 +0300

    ğ

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-let photo = document.getElementById("erenPhoto");
-let previous = document.getElementById("previous");
-console.log(previous);
-let next = document.getElementById("next");
-
-let control = 0;
+let control = 1;
 
 function Gallery(){
-    switch(Math.abs(control%7)){
-        case 0:
-            photo.src = "./img/Gallery/1.png";
-            break;
-        case 1:
-            photo.src = "./img/Gallery/2.png";
-            photo.
-            break;
-        case 2:
-            photo.src = "./img/Gallery/3.png";
-            break;
-        case 3:
-            photo.src = "./img/Gallery/4.png";
-            break;
-        case 4:
-            photo.src = "./img/Gallery/5.png";
-            break;
-        case 5:
-            photo.src = "./img/Gallery/6.png";
-            break;
-         case 6:
-            photo.src = "./img/Gallery/7.png";
-            break;
-    }
+    $(erenPhoto).attr("src", `./img/Gallery/${Math.abs(control%7)}.png`)
 }
 
-next.addEventListener("click", function() {control++; Gallery(); console.log(control)});
-previous.addEventListener("click", function() {control--; Gallery(); console.log(control)});
 
-
+$(next).click(function() {control++; if(Math.abs(control%7) === 0)control = 1; Gallery();});
+$(previous).click(function() {control--; if(Math.abs(control%7) === 0)control = -1; Gallery();});
